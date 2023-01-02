650228851ad10874f721c0bd1a81b23ccac9e06c
AlexandreSout0 <soutoinf@gmail.com>
2023-01-02 16:59:30 -0300

    cards with conection db

--- src/componentes/evento-card/index.js
+++ src/componentes/evento-card/index.js
@@ -1,32 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
-
+import {storage} from '../../config/firebase';
 import './evento-card.css';
 
 
 
-function EventoCard(){
-    return(
-        <div className = "col-md-3 col-sm-12" >
-            <img src = "https://via.placeholder.com/500" className="card-img-top img-cartao" alt="Imagem do Evento" />
+function EventoCard({id,img,titulo,detalhes,vizualizacoes}){
+    
+    const[urlImagem, setUrlImagem] = useState();
+    useEffect(() => {
+        storage.ref(`imagens/$/(img)`).getDownloadURL().then( url => setUrlImagem(url));
+    });
 
-            <div className = "card-body">
-                <h5>Título do Evento</h5>
-                <p className="card-text text-justify">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                
-                <div className="row rodape-card d-flex align-items-center">
-                    <div className = "col-6">
-                        <Link to='/' className = "btn btn-sm btn-detalhes">+ detalhes </ Link>
-                    </div>
+    return(
+            <div className = "col-md-3 col-sm-12" >
+                <img src = {urlImagem} className="card-img-top img-cartao" alt="Imagem do Evento" />
+
+                <div className = "card-body">
+                    <h5>{titulo}</h5>
+                    <p className="card-text text-justify">
+                        {detalhes}
+                    </p>
                     
-                    <div className="col-6 text-right">
-                        <i class="fa-solid fa-eye"></i><span>2019</span>
+                    <div className="row rodape-card d-flex align-items-center">
+                        <div className = "col-6">
+                            <Link to={'/eventodetalhe/' + id} className = "btn btn-sm btn-detalhes">+ detalhes </ Link>
+                        </div>
+                        
+                        <div className="col-6 text-right">
+                            <i class="fa-solid fa-eye"></i> <span>{vizualizacoes}</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         )
 }
 
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,31 +1,46 @@
 // Bibliotecas
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './home.css';
 import {Link} from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import firebase, { db } from '../../config/firebase.js';
 
 // Componentes
-import Navbar from '../../componentes/navbar/index';
-import EventoCard from '../../componentes/evento-card/index';
+import Navbar from '../../componentes/navbar/index.js';
+import EventoCard from '../../componentes/evento-card/index.js';
 
 
  function Home() {
+
+    const [eventos, setEventos] = useState([]);
+    let listaEventos = [];
+
+    useEffect(() => {
+        db.collection('eventos').get().then(async (resultado) => {
+            await resultado.docs.forEach( doc => {
+                listaEventos.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+            setEventos(listaEventos);
+        })
+    });
+
+
     return(
         <>
-        <Navbar />
-        <h1> {useSelector (state => state.usuarioEmail)} </h1>
-        
-        <div className="row">
-            <EventoCard />
-        </div>
-        
-        
-
-
-
+            <Navbar />
+            <h1> {useSelector (state => state.usuarioEmail)} </h1>
+            
+            <div className="row p-3">
+                { eventos.map ( item => <EventoCard key = {item.id} id = {item.id} img = {item.foto} titulo = {item.titulo} detalhes = {item.detalhes} vizualizacoes = {item.vizualizacoes} />) }
+            </div>
+            
         </>
     )
 
  }
+//
 
  export default Home;
